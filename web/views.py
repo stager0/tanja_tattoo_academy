87b45bf06c5c69e4dd1d8bad86058422459ebebc
Andrii Shtaher <andriishtaher@gmail.com>
2025-07-08 20:33:49 +0200

    feat: implement RegisterView with logic (when user is created, code will be automatically ativated and will stored activated_date)

--- web/views.py
+++ web/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.utils import timezone
+from django.views.generic import CreateView
 
-# Create your views here.
+from web.forms import CustomRegisterForm
+
+
+class RegisterView(CreateView):
+    form_class = CustomRegisterForm
+    template_name = "registration/register.html"
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        form.save()
+        code_obj = form.cleaned_data["code"]
+        code_obj.is_activated = True
+        code_obj.activated_date = timezone.now()
+        code_obj.save()
+
+        return redirect("login")

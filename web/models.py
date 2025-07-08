f1d6b101254b36f1411d57389aab37ba9bb56dca
Andrii Shtaher <andriishtaher@gmail.com>
2025-07-08 20:31:05 +0200

    feat: implement models Code and UserModel

--- web/models.py
+++ web/models.py
@@ -1,6 +1,20 @@
-from web.custom_auth_user_manager import EmailAbstractUser
+from django.core.validators import MinLengthValidator
+from django.db import models
+from django.utils import timezone
+
+from .custom_auth_user_manager import EmailAbstractUser
+
+
+class Code(models.Model):
+    code = models.CharField(max_length=14, unique=True, validators=[MinLengthValidator(14)])
+    is_activated = models.BooleanField(default=False, null=True, blank=True)
+    activated_date = models.DateTimeField(null=True, blank=True)
+    created_date = models.DateTimeField(default=timezone.now, null=True, blank=True)
 
 
 class UserModel(EmailAbstractUser):
+    code = models.ForeignKey(Code, related_name="users", on_delete=models.CASCADE)
+    phone = models.CharField(max_length=13, validators=[MinLengthValidator(13)], blank=True, null=True)
+
     def __str__(self):
         return self.email

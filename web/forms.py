c7695b8afbfe5b1a4daa6e8c2ba69aa33d8ed39b
Andrii Shtaher <andriishtaher@gmail.com>
2025-07-09 21:29:55 +0200

    feat: implement PasswordChangeRequestForm with email validation(clean method returns or email of an existing user or None)

--- web/forms.py
+++ web/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from django.contrib.auth.password_validation import validate_password
 from django.core.validators import MinLengthValidator
 
 from web.models import Code
@@ -13,7 +14,7 @@ class CustomRegisterForm(UserCreationForm):
     phone = forms.CharField(required=False, validators=[validate_phone_number])
 
     def clean_code(self):
-        code_str = self.cleaned_data["code"]
+        code_str = self.cleaned_data.get("code")
         try:
             code = Code.objects.get(code=code_str)
             return code
@@ -23,3 +24,27 @@ class CustomRegisterForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ["first_name", "last_name", "email", "password1", "password2", "phone", "code"]
+
+
+class PasswordChangeRequestForm(forms.Form):
+    email = forms.EmailField(required=True)
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        try:
+            user = get_user_model().objects.get(email=email)
+            return email
+        except get_user_model().DoesNotExist:
+            return None
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get("email", "")
+        if email:
+            user = get_user_model().objects.get(email=email)
+            full_name = user.get_full_name()
+            cleaned_data["full_name"] = full_name
+        else:
+            cleaned_data["full_name"] = None
+
+        return cleaned_data

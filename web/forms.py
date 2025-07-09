ae7c25bae448195044b0b071c159790ee1dcfc14
Andrii Shtaher <andriishtaher@gmail.com>
2025-07-09 21:32:52 +0200

    feat: implement ChangePasswordForm with password validation (build-in django) and also compare password1 and password2

--- web/forms.py
+++ web/forms.py
@@ -48,3 +48,23 @@ class PasswordChangeRequestForm(forms.Form):
             cleaned_data["full_name"] = None
 
         return cleaned_data
+
+
+class ChangePasswordForm(forms.Form):
+    code = forms.CharField(required=True, min_length=6, max_length=6)
+    password1 = forms.CharField(required=True, min_length=8, max_length=50)
+    password2 = forms.CharField(required=True, min_length=8, max_length=50)
+
+    def clean_password1(self):
+        validate_password(self.cleaned_data["password1"])
+        return self.cleaned_data["password1"]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        pass1 = cleaned_data.get("password1", "")
+        pass2 = cleaned_data.get("password2", "")
+
+        if not pass1 or not pass2 or pass1 != pass2:
+            raise forms.ValidationError("Паролі не співпадають.")
+
+        return cleaned_data

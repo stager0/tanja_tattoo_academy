50a10c1ea454d7a13ddee4beff9f52b54ca0cd1c
Andrii Shtaher <andriishtaher@gmail.com>
2025-07-09 21:26:45 +0200

    feat: implement ResetCode model, also add  get_full_name method to usermodel

--- web/models.py
+++ web/models.py
@@ -5,6 +5,13 @@ from django.utils import timezone
 from .custom_auth_user_manager import EmailAbstractUser
 
 
+class ResetCode(models.Model):
+    code = models.CharField(max_length=6, validators=[MinLengthValidator(6)])
+    user_email = models.EmailField(blank=False, null=False)
+    is_activated = models.BooleanField(default=False, blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now, null=True, blank=True)
+
+
 class Code(models.Model):
     code = models.CharField(max_length=14, unique=True, validators=[MinLengthValidator(14)])
     is_activated = models.BooleanField(default=False, null=True, blank=True)
@@ -18,3 +25,7 @@ class UserModel(EmailAbstractUser):
 
     def __str__(self):
         return self.email
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+

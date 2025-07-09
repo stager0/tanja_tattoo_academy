c1d607cf6033cd17e6873893aaa6cd3bac507d8a
Andrii Shtaher <andriishtaher@gmail.com>
2025-07-09 21:23:32 +0200

    feat: update logic in RegisterView

--- web/views.py
+++ web/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render, redirect
+from datetime import timedelta
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.utils import timezone
+from django.views import generic
 from django.views.generic import CreateView
 
-from web.forms import CustomRegisterForm
+from web.email_sender import send_password_change_email, send_after_register_email
+from web.forms import CustomRegisterForm, PasswordChangeRequestForm, ChangePasswordForm
+from web.generators import generate_reset_password_code
+from web.models import ResetCode
 
 
 class RegisterView(CreateView):
@@ -17,5 +24,15 @@ class RegisterView(CreateView):
         code_obj.is_activated = True
         code_obj.activated_date = timezone.now()
         code_obj.save()
+        code_obj = form.cleaned_data.get("code")
+        if code_obj:
+            code_obj.is_activated = True
+            code_obj.activated_date = timezone.now()
+            code_obj.save()
 
         return redirect("login")
+        email = form.cleaned_data.get("email", "")
+        full_name = form.cleaned_data.get("first_name", "") + " " + form.cleaned_data.get("last_name", "")
+        if email and full_name:
+            send_after_register_email(email=email, full_name=full_name)
+        return super().form_valid(form)

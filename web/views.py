93703f03bbbc7c25f5efd48a4802f93e1a4a25ef
Andrii Shtaher <andriishtaher@gmail.com>
2025-07-14 23:51:29 +0200

    feat: implement get_context_data in AdminAllChatsView

--- web/views.py
+++ web/views.py
@@ -724,3 +724,12 @@ class AdminAllChatsView(LoginRequiredMixin, generic.ListView):
 
         return queryset
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        count_of_waiting = HomeWork.objects.filter(was_checked=False).count()
+        count_of_new_messages = Message.objects.filter(is_read_admin=False).count()
+
+        context["count_of_waiting"] = count_of_waiting
+        context["count_of_new_messages"] = count_of_new_messages
+
+        return context

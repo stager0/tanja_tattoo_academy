8e9f6d9c34d85fc8d2d971665436437a9e64b2ea
Andrii Shtaher <andriishtaher@gmail.com>
2025-07-17 23:45:33 +0200

    feat: implement generate_subscribe_code method that returns random subscribe_code

--- web/generators.py
+++ web/generators.py
@@ -1,4 +1,16 @@
+import random
 import secrets
+from string import ascii_uppercase
+
 
 def generate_reset_password_code(length=6) -> str:
     return "".join(str(secrets.randbelow(10)) for _ in range(length))
+
+def generate_subscribe_code(length=14):
+    code = ""
+    for _ in range(length):
+        if len(code) == 4 or len(code) == 9:
+            code += "-"
+        else:
+            code += random.choice(ascii_uppercase)
+    return code

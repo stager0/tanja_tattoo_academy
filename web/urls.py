e3977137b6adbd93d2c0e306ec1275865fe09c91
Andrii Shtaher <andriishtaher@gmail.com>
2025-07-27 15:42:49 +0200

    feat(urls): add load_more_messages endpoint

--- web/urls.py
+++ web/urls.py
@@ -1,16 +1,14 @@
 from django.urls import path
-from django.views.generic import TemplateView
 
-from web.views import DashboardView, ChatView, ProfileView, CourseView, AdminReviewListView, AdminReviewTaskView, \
+from web.views import DashboardView, ChatView, ProfileUpdateView, CourseView, AdminReviewListView, AdminReviewTaskView, \
     AdminDashboardView, AdminStudentsView, AdminBoxesView, BoxApplicationView, AdminLectureList, AdminLectureEditView, \
-    AdminLectureCreateView, AdminAllChatsView, AdminLectureDelete
+    AdminLectureCreateView, AdminAllChatsView, AdminLectureDelete, get_part_of_messages
 
 urlpatterns = [
-    path("thank_you/", TemplateView.as_view(template_name="thank-you.html"), name="thank_you"),
     path("dashboard/", DashboardView.as_view(), name="dashboard"),
     path("chat/<int:pk>/", ChatView.as_view(), name="chat"),
     path("chat_admin/<int:pk>/", ChatView.as_view(), name="admin_chat"),
-    path("profile/", ProfileView.as_view(), name="profile"),
+    path("profile/", ProfileUpdateView.as_view(), name="profile"),
     path("course/<int:pk>/", CourseView.as_view(), name="course"),
     path("admin_review_list/", AdminReviewListView.as_view(), name="admin_review_list"),
     path("admin_review_task/<int:pk>/", AdminReviewTaskView.as_view(), name="admin_review_task"),
@@ -22,5 +20,6 @@ urlpatterns = [
     path("admin_lecture_edit/<int:pk>/", AdminLectureEditView.as_view(), name="admin_lecture_edit"),
     path("admin_lecture_create/", AdminLectureCreateView.as_view(), name="admin_lecture_create"),
     path("admin_lecture_delete/<int:pk>/", AdminLectureDelete.as_view(), name="admin_lecture_delete"),
-    path("admin_all_chats/", AdminAllChatsView.as_view(), name="admin_all_chats")
+    path("admin_all_chats/", AdminAllChatsView.as_view(), name="admin_all_chats"),
+    path("load_more_messages/<int:pk>/",  get_part_of_messages, name="load_more_messages")
 ]

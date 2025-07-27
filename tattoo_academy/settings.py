fb94cf7e11b444e0a7d5e063cd7b06fef328822e
Andrii Shtaher <andriishtaher@gmail.com>
2025-07-27 22:32:09 +0200

    feat(settings): add TELEGRAM_BOT_TOKEN and HOST variables in settings.py

--- tattoo_academy/settings.py
+++ tattoo_academy/settings.py
@@ -25,14 +25,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_KEY")
+
+HOST = os.getenv("HOST")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv('HOST'), "127.0.0.1"]
+ALLOWED_HOSTS = [HOST, "127.0.0.1"]
 
 CSRF_TRUSTED_ORIGINS = [
-    f"https://{os.getenv('HOST')}"
+    f"https://{HOST}",
 ]
 
 

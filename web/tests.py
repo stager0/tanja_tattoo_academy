19d499441e0c711adb9101859936c707222720af
Andrii Shtaher <andriishtaher@gmail.com>
2025-07-28 19:13:50 +0200

    test: implement test for register

--- web/tests.py
+++ web/tests.py
@@ -1,5 +1,8 @@
+from multiprocessing.resource_tracker import register
+
 import pytest
 from django.urls import reverse
+from django.utils import timezone
 
 from web.models import UserModel, Order, SubscribeTariff, Code
 
@@ -16,6 +19,17 @@ def admin_user(db):
     return admin
 
 @pytest.fixture
+def user(db):
+    user = UserModel.objects.create_user(
+        first_name="Antonino",
+        last_name="Bombardino",
+        telegram_chat_id="2222222222",
+        email="user@user.com"
+    )
+    user.set_password("1qaz")
+    return user
+
+@pytest.fixture
 def subscribe_tariff_base(db):
     return SubscribeTariff.objects.create(
         name="base",
@@ -24,9 +38,36 @@ def subscribe_tariff_base(db):
     )
 
 @pytest.fixture
+def subscribe_tariff_master(db):
+    return SubscribeTariff.objects.create(
+        name="master",
+        price="750",
+        with_startbox=True
+    )
+
+@pytest.fixture
 def order(db, subscribe_tariff_base):
     price = SubscribeTariff.objects.get(name="base").price
     return Order.objects.create(total_sum=price)
+
+@pytest.fixture
+def order_paid_master(db, subscribe_tariff_master, user):
+    return Order.objects.create(
+        total_sum=subscribe_tariff_master.price,
+        is_paid=True,
+        user_email=user.email,
+        session_id="361253762154"
+    )
+
+@pytest.fixture
+def code_master(db, order_paid_master):
+    return Code.objects.create(
+        code="ASDF-ASDF-ASDF",
+        order=order_paid_master,
+        tariff="master"
+    )
+
+# -------------------------TESTS---------------------------
 
 @pytest.mark.django_db
 def test_send_index_form_valid_status_302(client, admin_user, mocker):
@@ -94,6 +135,9 @@ def test_stripe_webhook_changes_order_and_creates_new_code_also_sends_email(db, 
             }
         }
     }
+    mocker_mailjet = mocker.patch("web.views.send_email_subscribe_code")
+    mocker_mailjet.return_value = None
+
     response = client.post(reverse("webhook"))
     order.refresh_from_db()
 
@@ -105,5 +149,32 @@ def test_stripe_webhook_changes_order_and_creates_new_code_also_sends_email(db, 
     assert order.session_id == session_id
     assert new_code.order == order
     assert new_code.tariff == subscribe_tariff_base.name
+    mocker_mailjet.assert_called_once()
 
+
+@pytest.mark.django_db
+def test_register_user_with_code(db, client, code_master, user):
+    data = {
+        "first_name": "Dmytro",
+        "last_name": "Ukrainets",
+        "email": "test@lerner.com",
+        "password1": "1qazxsw23edcvfr4-=",
+        "password2": "1qazxsw23edcvfr4-=",
+        "code": code_master.code,
+    }
+    response = client.post(reverse("register"), data=data)
+
+    new_user = UserModel.objects.filter(email="test@lerner.com").first()
+
+    assert response.status_code == 302
+
+    assert new_user.first_name == "Dmytro"
+    assert new_user.last_name == "Ukrainets"
+    assert new_user.code == code_master
+
+    code_master.refresh_from_db()
+
+    assert code_master.activated_date.timestamp() == pytest.approx(timezone.now().timestamp())
+    assert code_master.is_activated == True
+    assert code_master.start_box_coupon_is_activated == False
 

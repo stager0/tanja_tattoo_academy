d04954202f82a4182c7cf42e1b900f391f8053c7
Andrii Shtaher <andriishtaher@gmail.com>
2025-07-30 17:41:01 +0200

    feat(validators): correct validate_phone_number and validate_subscribe_code

--- web/validators.py
+++ web/validators.py
@@ -1,14 +1,16 @@
-from django.core.exceptions import ValidationError
+from django import forms
 
 from web.models import Code
 
 
-def validate_phone_number(number: str) -> ValidationError | None:
-    if not len(number) == 13 and (number[:4] == "+380" or number[:3] == "+49"):
-        raise ValidationError("Phone number must be 13 symbols lang.")
+def validate_phone_number(number: str) -> None:
+    if number.startswith("+380") and len(number) != 13:
+        raise forms.ValidationError("Номер телефону України має містити 13 символів (включаючи +).")
+    elif number.startswith("+49") and len(number) != 13:
+        raise forms.ValidationError("Німецький номер телефону має містити 13 символів.")
+    elif not (number.startswith("+380") or number.startswith("+49")):
+        raise forms.ValidationError("Номер має починатися з +380 або +49.")
 
-
-def validate_subscribe_code(code: str) -> ValidationError | None:
-    code = Code.objects.get(code=code)
-    if not code or code.is_activated is True:
-        raise ValidationError("Invalid code was provided.")
+def validate_subscribe_code(code: str) -> Code | None:
+    code_obj = Code.objects.filter(code=code).first()
+    return code_obj if code_obj else None

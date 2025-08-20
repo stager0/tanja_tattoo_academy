d5f2190345ddd3003a66b0f5c5a954181421ceb6
Andrii <andriishtaher@gmail.com>
2025-08-21 00:30:44 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,5 @@ python manage.py collectstatic --no-input --clear
 echo "Loading vital fixture data..."
 python manage.py loaddata fixture_tariffs.json
 
-echo "Setting Telegram webhook..."
-python manage.py shell -c "from web.telegram_bot import set_telegram_webhook; set_telegram_webhook()"
-
 echo "Starting Gunicorn server..."
 exec gunicorn tattoo_academy.wsgi:application --bind 0.0.0.0:8000
